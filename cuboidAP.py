4fe22d19106504f826bb9fcac1516c925b9050a4
Aloshya001 <76680129+Aloshya001@users.noreply.github.com>
2021-02-04 14:53:11 +0530

    Add files via upload

---
+++ cuboidAP.py
@@ -0,0 +1,8 @@
+#area and perimeter of cuboid
+from cuboidAPFunction import *
+a=int(input("enter the  edge: "))
+l=int(input("enter the length: "))
+b=int(input("enter the breadth: "))
+h=int(input("enter the height: "))
+print("area of cuboid is : ", Acuboid(a))
+print("perimetr of cubiod is : ",Pcuboid(l,b,h))
