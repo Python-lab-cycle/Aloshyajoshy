86a4a8ff6a65c362675390eab270a92c5dbf4b31
Aloshya001 <76680129+Aloshya001@users.noreply.github.com>
2021-02-06 14:51:24 +0530

    Add files via upload

---
+++ sphereAPFunction.py
@@ -0,0 +1,10 @@
+#area and perimeter of sphere fun
+from math import pi
+def Asphere(r):
+    result=4*pi*r*r
+    return result
+
+#perimeter fun
+def Psphere(r):
+    result=(4/3)*pi*r*r*r
+    return result
